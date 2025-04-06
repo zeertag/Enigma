6117194be2dd527a7a915d9ba63a73402d38ad10
zeertag <taras0705@yandex.ru>
2025-04-06 17:51:00 +0300

    предварительно готово

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import Rotors
 
 
-# letter = ord(input()) - 64
-# a = RotorI().go_through(letter)
-# print(chr(a + 64))
-
-# s = input()
-# for i in range(len(s)):
-#     print(f"{i + 1}: {ord(s[i]) - 64},")
-
-
 class Machine:
     def __init__(self):
         self.ETW = {
@@ -78,26 +69,68 @@ class Machine:
 
         self.choice_rotors = None
         self.rotor_pos = None
+        self.commutations = {}
+
+    def check_rotors(self):
+        for i in self.choice_rotors:
+            if i < 1 or i > 5:
+                print("Некорректный ввод параметров")
+                return 1
+        return 0
+
+    def check_poses(self):
+        for i in self.rotor_pos:
+            if i < 0 or i > 25:
+                print("Некорректный ввод параметров")
+                return 1
+        return 0
 
     def settings(self):
-        self.choice_rotors = list(map(int, input("Выберите 3 ротора (1-5): ").split(' ')))
-        self.rotor_pos = list(
-            map(lambda x: int(x) - 1, input("Выберите стартовые положения для роторов (1-26): ").split()))
-        print("коммутации потом")
+        f = 1
+        while f:
+            self.choice_rotors = list(map(int, input("Выберите 3 ротора (1-5): ").split(' ')))
+            f = self.check_rotors()
+
+        f = 1
+        while f:
+            self.rotor_pos = list(
+                map(lambda x: int(x) - 1, input("Выберите стартовые положения для роторов (1-26): ").split()))
+            f = self.check_poses()
+
+        self.rotor_pos.append(0)
+        print()
+        c = -1
+        while c != 1 and c != 0:
+            c = int(input("Подключить коммутации?(1-да, 0-нет): "))
+        if c:
+            alphabet = [chr(_) for _ in range(65, 91)]
+            print()
+            print("Выбирайте по 2 буквы из алфавита и записывайте в формате буква буква (например A B)")
+            print("Для завершения ввода ввести 0")
+            print('------')
+            while len(alphabet) >= 2:
+                print(alphabet)
+                connect = input("Введите соединение: ")
+                if connect == "0":
+                    break
+
+                connect = connect.split(' ')
+                self.commutations[ord(connect[0]) - 64] = ord(connect[-1]) - 64
+                self.commutations[ord(connect[-1]) - 64] = ord(connect[0]) - 64
+                alphabet.remove(connect[0])
+                alphabet.remove(connect[1])
 
     def rotate(self, rotor):
-        if self.rotors[rotor].notch < self.rotor_pos[rotor - 1]:
+        if self.rotors[rotor].notch == self.rotor_pos[rotor - 1] - 1:
             self.rotor_pos[rotor - 2] += 1
 
-    def go(self, s, rotor):
-        p = s + self.rotor_pos[rotor - 1]
+    def go(self, s, rotor, r):
+        p = s + (self.rotor_pos[r - 1] - self.rotor_pos[r])
         if p > 26:
             p -= 26
+        elif p < 1:
+            p += 26
         s = self.rotors[rotor].go_through(p)
-        # if p % 26 == 0:
-        #     s = self.rotors[rotor].go_through(p % 26 + 1)
-        # else:
-        #     s = self.rotors[rotor].go_through(p % 26)
         return s
 
     def key_for_data(self, data, rotor):
@@ -107,6 +140,13 @@ class Machine:
                 return i
         return "Ошибка"
 
+    def balance(self, s):
+        if s > 26:
+            s -= 26
+        elif s < 1:
+            s += 26
+        return s
+
     def coding(self, letter):
         num_code = self.letters_to_numbers(letter)
         new_letter = ''
@@ -115,33 +155,41 @@ class Machine:
                 new_letter += symbol
             else:
                 # сигнал вперед
-                s = self.ETW[symbol]
+                if symbol in self.commutations:
+                    s = self.commutations[symbol]
+                    s = self.ETW[s]
+                else:
+                    s = self.ETW[symbol]
                 for rotor in range(len(self.choice_rotors), 0, -1):
                     if rotor == 3:
                         self.rotor_pos[rotor - 1] += 1
                         self.rotate(rotor)
                         self.rotate(rotor - 1)
 
-                        for i in range(len(self.rotor_pos)):
+                        for i in range(len(self.rotor_pos) - 1):
                             if self.rotor_pos[i] == 27:
                                 self.rotor_pos[i] = 1
 
-                    # p = s + self.rotor_pos[rotor - 1]
-                    # if p % 26 == 0:
-                    #     s = self.rotors[rotor].go_through(p % 26 + 1)
-                    # else:
-                    #     s = self.rotors[rotor].go_through(p % 26)
-                    s = self.go(s, rotor)
-                    # s -= self.rotor_pos[rotor - 1]
+                    s = self.go(s, self.choice_rotors[rotor - 1], rotor)
 
                 # сигнал назад
+                s += 0 - self.rotor_pos[0]
+                s = self.balance(s)
                 s = self.UKW[s]
-                for rotor in range(1, len(self.choice_rotors) + 1):
-                    s += self.rotor_pos[rotor - 1]
-                    if s > 26:
-                        s -= 26
-                    s = self.key_for_data(s, rotor)
-                    # s = self.go(s, rotor)
+
+                for i in range(len(self.choice_rotors)):
+                    if i == 0:
+                        s += self.rotor_pos[i]
+                    else:
+                        s += self.rotor_pos[i] - self.rotor_pos[i - 1]
+                    s = self.balance(s)
+                    s = self.key_for_data(s, self.choice_rotors[i])
+
+                s += 0 - self.rotor_pos[2]
+                s = self.balance(s)
+
+                if s in self.commutations:
+                    s = self.commutations[s]
 
                 new_letter += chr(s + 64)
 
